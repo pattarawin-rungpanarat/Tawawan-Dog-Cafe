c7fcfe75c8d3d7c9a2d7016e508b0794c7d80840
pattarawin-rungpanarat <67026427@up.ac.th>
2025-02-09 13:12:04 +0700

    sss

--- SigninwithGooglePassword/SigninwithGooglePassword.js
+++ SigninwithGooglePassword/SigninwithGooglePassword.js
@@ -1,23 +1,38 @@
 function validatePassword() {
     let password = document.getElementById("password").value;
-    let passwordError = document.getElementById("password-error");
+    let errorMessage = document.getElementById("password-error");
     if (password.length < 8) {
-        passwordError.innerText = "รหัสผ่านไม่ถูกต้อง ลองอีกครั้งหรือคลิก ลืมรหัสผ่าน เพื่อรีเซ็ตรหัส";
-        document.getElementById("password").style.border = "2px solid red";
+        errorMessage.innerText = "";
+        document.getElementById("password").style.border = "1px solid #ccc";
     } else {
-        passwordError.innerText = "";
+        errorMessage.innerText = "";
         document.getElementById("password").style.border = "1px solid #ccc";
     }
 }
 document.getElementById("toggle-password").addEventListener("click", function () {
     const passwordInput = document.getElementById("password");
     const toggleIcon = document.getElementById("toggle-password");
-
     if (passwordInput.type === "password") {
         passwordInput.type = "text";
         toggleIcon.classList.replace("bx-square", "bx-check-square");
     } else {
         passwordInput.type = "password";
         toggleIcon.classList.replace("bx-check-square", "bx-square");
+    }
+});
+document.querySelector(".next-bnt").addEventListener("click", function (event) {
+    let password = document.getElementById("password").value.trim();
+    let passwordError = document.getElementById("password-error").innerText.trim();
+    let errorMessage = document.getElementById("password-error");
+    if (password === "") {``
+        errorMessage.innerText = "❗ป้อนรหัสผ่าน";
+        document.getElementById("password").style.border = "2px solid red";
+        event.preventDefault();
+    } else if (passwordError !== ""){
+        errorMessage.innerText = "❗รหัสผ่านไม่ถูกต้อง ลองอีกครั้งหรือคลิก ลืมรหัสผ่าน เพื่อรีเซ็ตรหัส";
+        document.getElementById("password").style.border = "2px solid red";
+        event.preventDefault();
+    } else{
+        return;
     }
 });

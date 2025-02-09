df5fb228eb0ad259ddb9923d0a3369beea98a594
pattarawin-rungpanarat <67026427@up.ac.th>
2025-02-10 02:01:46 +0700

    log out

--- PersonalInformation/PersonalInformation.js
+++ PersonalInformation/PersonalInformation.js
@@ -1,2 +1,6 @@
+function logout() {
+    alert("ออกจากระบบเรียบร้อย!");
+    window.location.href = "../login/login.html";
+}
 let registeredaccount = localStorage.getItem("registeredaccount");
 document.getElementById("account-btn").textContent = registeredaccount;

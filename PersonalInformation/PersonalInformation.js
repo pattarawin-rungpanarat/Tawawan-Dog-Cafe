e0bbf1019425ee49d61dfd7eea6ff024aa748b27
pattarawin-rungpanarat <67026427@up.ac.th>
2025-02-12 20:04:18 +0700

    ชื่อด้านบน

--- PersonalInformation/PersonalInformation.js
+++ PersonalInformation/PersonalInformation.js
@@ -2,14 +2,6 @@ function logout() {
     alert("ออกจากระบบเรียบร้อย!");
     window.location.href = "../Frist/frist.html";
 }
-document.addEventListener("DOMContentLoaded", function() {
-    let btn = document.getElementById("account-btn");
-    let text = btn.innerText;
-
-    if (text.length > 5) {
-        btn.innerText = text.substring(0, 5) + "...";
-    }
-});
 let registeredaccount = localStorage.getItem("registeredaccount");
 document.getElementById("account-btn").textContent = registeredaccount;
 
@@ -117,4 +109,12 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
     toggleIcon.addEventListener("click", togglePasswordVisibility);
+});
+document.addEventListener("DOMContentLoaded", function() {
+    let btn = document.getElementById("account-btn");
+    let text = btn.innerText;
+
+    if (text.length > 5) {
+        btn.innerText = text.substring(0, 5) + "...";
+    }
 });

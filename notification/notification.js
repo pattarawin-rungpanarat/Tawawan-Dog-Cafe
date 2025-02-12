628b25c205fb139cfbe727205da5c9081f7c3011
pattarawin-rungpanarat <67026427@up.ac.th>
2025-02-12 22:42:30 +0700

    btn

--- notification/notification.js
+++ notification/notification.js
@@ -1,2 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let btn = document.getElementById("account-btn");
+    let text = btn.innerText;
+
+    if (text.length > 5) {
+        btn.innerText = text.substring(0, 5) + "...";
+    }
+});
 let registeredaccount = localStorage.getItem("registeredaccount");
 document.getElementById("account-btn").textContent = registeredaccount;

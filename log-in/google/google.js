d7f183839629889e38b7f3f39ef271e7a5a10591
pattarawin-rungpanarat <67026427@up.ac.th>
2025-02-16 02:39:22 +0700

    go

--- log-in/google/google.js
+++ log-in/google/google.js
@@ -1,13 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     let btn = document.getElementById("avatar-btn");
-    let text = btn.innerText;
+    let registeredaccount = localStorage.getItem("registeredaccount");
 
-    if (text.length > 1) {
-        btn.innerText = text.substring(0, 2);
+    if (registeredaccount.length > 1) {
+        btn.innerText = registeredaccount.substring(0, 2);
     }
 });
 let registeredaccount = localStorage.getItem("registeredaccount");
-document.getElementById("avatar-btn").textContent = registeredaccount;
 document.getElementById("email").innerText = localStorage.getItem("registeredemail");
-
-

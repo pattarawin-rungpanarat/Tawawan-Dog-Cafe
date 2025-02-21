a4f672ae6d1e93224c102d1c6ba72fd5a7674b31
pattarawin-rungpanarat <67026427@up.ac.th>
2025-02-21 21:28:10 +0700

    จบ

--- PartnersDormyhome/ex-list/ex-list.js
+++ PartnersDormyhome/ex-list/ex-list.js
@@ -1,3 +1,16 @@
+function loadImagesOnOtherPage() {
+    for (let i = 1; i <= 5; i++) {
+        const previewId = `preview${i}`;
+        const savedImage = localStorage.getItem(previewId);
+        if (savedImage) {
+            const imageContainer = document.getElementById(previewId);
+            if (imageContainer) {
+                imageContainer.innerHTML = `<img src="${savedImage}" alt="Saved Image">`;
+            }
+        }
+    }
+}
+window.onload = loadImagesOnOtherPage;
 function deleteRoom() {
     const mainBox = document.querySelector('.main');
     const area = document.querySelector('.main');

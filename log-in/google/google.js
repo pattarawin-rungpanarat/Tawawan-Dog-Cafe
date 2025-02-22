bf3e89bebfd385ed74ffe0be53879c8b6f4f8b05
pattarawin-rungpanarat <67026427@up.ac.th>
2025-02-22 22:58:59 +0700

    google

--- log-in/google/google.js
+++ log-in/google/google.js
@@ -8,3 +8,20 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 let registeredaccount = localStorage.getItem("registeredaccount");
 document.getElementById("email").innerText = localStorage.getItem("registeredemail");
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function changeAvatarColor() {
+    const avatar = document.querySelector('.avatar');
+    avatar.style.backgroundColor = getRandomColor();
+}
+
+document.addEventListener('DOMContentLoaded', changeAvatarColor);
+document.querySelector('.avatar').addEventListener('click', changeAvatarColor);
